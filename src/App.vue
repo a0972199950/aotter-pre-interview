bfd44d3a84212d1c7857fdcf727254b2d064ce94
a0972199950 <a0972199950@gmail.com>
2019-07-16 20:21:49 +0800

    完成svg icon

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
 
+    <img src="data:image/svg+xml;base64,
+PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB2ZXJzaW9uPSIxLjEiIGlkPSJDYXBhXzEiIHg9IjBweCIgeT0iMHB4IiB2aWV3Qm94PSIwIDAgNjAgNjAiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDYwIDYwOyIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSIgd2lkdGg9IjUxMiIgaGVpZ2h0PSI1MTIiIGNsYXNzPSIiPjxnPjxwYXRoIGQ9Ik02LDJoNDhjMy4yNTIsMCw2LDIuNzQ4LDYsNnYzM2MwLDMuMjUyLTIuNzQ4LDYtNiw2SDI1LjQ0MkwxNS43NCw1Ny42NzNDMTUuNTQ2LDU3Ljg4NSwxNS4yNzYsNTgsMTUsNTggIGMtMC4xMjEsMC0wLjI0My0wLjAyMi0wLjM2MS0wLjA2N0MxNC4yNTQsNTcuNzg0LDE0LDU3LjQxMywxNCw1N1Y0N0g2Yy0zLjI1MiwwLTYtMi43NDgtNi02TDAsOEMwLDQuNzQ4LDIuNzQ4LDIsNiwyeiIgZGF0YS1vcmlnaW5hbD0iIzAwMDAwMCIgY2xhc3M9ImFjdGl2ZS1wYXRoIiBzdHlsZT0iZmlsbDojRkZGRkZGIiBkYXRhLW9sZF9jb2xvcj0iIzAwMDAwMCI+PC9wYXRoPjwvZz4gPC9zdmc+" />
     
+    <!--
     <a href="google.com" class="ad-banner">
       <img class="ad-banner__img" src="https://agirls.aotter.net/media/da724a8b-fe19-4f4e-8262-75c207ae038b.jpg">
       <div class="ad-banner__content">
@@ -11,9 +14,9 @@
         <div class="ad-banner__icon">i</div>
       </div>
     </a>
-    
+    -->
 
-    <!--
+    
     <div class="ad-video">
       <img class="ad-video__img" src="https://agirls.aotter.net/media/60dcde35-6798-4784-8985-78323c7ec75b.jpg">
 
@@ -21,16 +24,38 @@
 
       <a href="google.com">
         <div class="ad-video__btn">
-          <div class="ad-video__icon"></div>
+          <svg x="0px" y="0px" viewBox="0 0 232.153 232.153" xml:space="preserve" class="ad-video__icon">
+            <path
+              d="M203.791,99.628L49.307,2.294c-4.567-2.719-10.238-2.266-14.521-2.266   c-17.132,0-17.056,13.227-17.056,16.578v198.94c0,2.833-0.075,16.579,17.056,16.579c4.283,0,9.955,0.451,14.521-2.267   l154.483-97.333c12.68-7.545,10.489-16.449,10.489-16.449S216.471,107.172,203.791,99.628z" fill="#FFFFFF" />
+          </svg>
         </div>
       </a>
 
       <div class="ad-video__social">
-        <div>&#128077;195</div>
-        <div>&#128172;14</div>
+
+        <div>
+          <svg x="0px" y="0px" viewBox="0 0 511.999 511.999" xml:space="preserve">            
+            <path
+              d="M83.578,167.256H16.716C7.524,167.256,0,174.742,0,183.971v300.881c0,9.225,7.491,16.713,16.716,16.713h66.862    c9.225,0,16.716-7.489,16.716-16.713V183.971C100.294,174.742,92.769,167.256,83.578,167.256z"
+              data-original="#ffffff" data-old_color="#ffffff" fill="#ffffff" />
+            <path
+              d="M470.266,167.256c-2.692-0.456-128.739,0-128.739,0l17.606-48.032c12.148-33.174,4.283-83.827-29.424-101.835    c-10.975-5.864-26.309-8.809-38.672-5.697c-7.09,1.784-13.321,6.478-17.035,12.767c-4.271,7.233-3.83,15.676-5.351,23.696    c-3.857,20.342-13.469,39.683-28.354,54.2c-25.952,25.311-106.571,98.331-106.571,98.331v267.45h278.593    c37.592,0.022,62.228-41.958,43.687-74.749c22.101-14.155,29.66-43.97,16.716-66.862c22.102-14.155,29.66-43.97,16.716-66.862    C527.572,235.24,514.823,174.792,470.266,167.256z"
+                    data-original="#ffffff" data-old_color="#ffffff" fill="#ffffff" />
+          </svg>
+          195
+        </div>
+
+        <div>
+          <svg x="0px" y="0px" viewBox="0 0 60 60" style="enable-background:new 0 0 60 60;" xml:space="preserve">
+              <path
+                  d="M6,2h48c3.252,0,6,2.748,6,6v33c0,3.252-2.748,6-6,6H25.442L15.74,57.673C15.546,57.885,15.276,58,15,58  c-0.121,0-0.243-0.022-0.361-0.067C14.254,57.784,14,57.413,14,57V47H6c-3.252,0-6-2.748-6-6L0,8C0,4.748,2.748,2,6,2z"
+                  data-original="#000000" data-old_color="#000000" fill="#FFFFFF" />
+          </svg>
+          14
+        </div>
       </div>
     </div>
-    -->
+    
     
     
     <div style="height: 1000px;"></div>
